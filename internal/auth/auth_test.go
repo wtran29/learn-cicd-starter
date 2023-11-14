11e711f7528a0525177196a59cfeb2c14954d81a
William Tran <wtran29@gmail.com>
2023-11-14 14:07:43 -0800

    added test, testing ci fail

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name          string
+		headers       http.Header
+		expectedKey   string
+		expectedError error
+	}{
+		{
+			name:          "Valid API key",
+			headers:       http.Header{"Authorization": []string{"ApiKey abc123"}},
+			expectedKey:   "abc123",
+			expectedError: nil,
+		},
+		{
+			name:          "No Authorization header",
+			headers:       http.Header{},
+			expectedKey:   "abc123",
+			expectedError: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name:          "Malformed Authorization header",
+			headers:       http.Header{"Authorization": []string{"Bearer token"}},
+			expectedKey:   "",
+			expectedError: errors.New("malformed authorization header"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			key, err := GetAPIKey(tt.headers)
+
+			if key != tt.expectedKey {
+				t.Errorf("Expected key %s, got %s", tt.expectedKey, key)
+			}
+
+			if err != tt.expectedError {
+				if err == nil || err.Error() != tt.expectedError.Error() {
+					t.Errorf("Expected error %v, got %v", tt.expectedError, err)
+				}
+			}
+		})
+	}
+}
